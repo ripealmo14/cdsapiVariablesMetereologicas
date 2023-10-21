4191e3ef4d334232310658c29e6213c7f14e1729
ripealmo14 <ricardo.aliaga.m@uni.pe>
2023-10-21 00:51:36 -0500

    Se agrega sh de ejecucion de scripts

---
+++ src/ejecucion.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+list_variable = ("cloud_cover" "snow_thickness_lwe" "2m_temperature" "2m_dewpoint_temperature" "snow_thickness" "vapour_pressure" "10m_wind_speed")
+list_mounth = ("01" "02" "03" "04" "05" "06" "07" "08" "09" "10" "11" "12")
+list_year = ("2018" "2019" "2020" "2021" "2022" "2023")
+
+for variable in "${list_variable[@]}"; do
+    for year in "${list_year[@]}"; do
+        for mounth in "${list_mounth[@]}"; do
+            /usr/bin/python3 CDSAPI_VAR_METEREOLOGICAS.py $mounth $colores $variable
+            sleep (5)
+done    
+done
+done
